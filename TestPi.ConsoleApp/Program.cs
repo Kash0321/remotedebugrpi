5c2bfddc173765aa28d1520be24509fac344b9d6
Kash <sergio.castillo@pronet-ise.com>
2019-08-13 11:57:08 +0200

    Demo configurada para depuración remota

---
+++ TestPi.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using Iot.Device.CpuTemperature;
+
+namespace TestPi.ConsoleApp
+{
+    class Program
+    {
+        static CpuTemperature CPUTemperatureSensor { get; set; } = new CpuTemperature();
+
+        static void Main(string[] args)
+        {
+            var pin = 17;
+            var controller = new GpioController();
+            controller.OpenPin(pin, PinMode.Output);
+
+            var lightTimeInMilliseconds = 1000;
+            var dimTimeInMilliseconds = 200;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Now entering IoT loop...");
+                        
+            while (true)
+            {
+                Console.WriteLine($"Light for {lightTimeInMilliseconds}ms");
+                controller.Write(pin, PinValue.High);
+                Thread.Sleep(lightTimeInMilliseconds);
+                Console.WriteLine($"Dim for {dimTimeInMilliseconds}ms");
+                controller.Write(pin, PinValue.Low);
+                Thread.Sleep(dimTimeInMilliseconds); 
+                if (CPUTemperatureSensor.IsAvailable)
+                {
+                    Console.WriteLine($"The CPU temperature is {CPUTemperatureSensor.Temperature.Celsius}");
+                }
+            }
+        }
+    }
+}
---
+++ TestPi.ConsoleApp/publish.bat
@@ -0,0 +1,8 @@
+:: dotnet publish -r linux-arm
+:: cd bin\Debug\netcoreapp2.2\linux-arm
+:: scp -r publish\*.* pi@harlequin.local:/home/pi/Work/dotnet/TestPi.ConsoleApp
+
+dotnet publish -r linux-arm /p:ShowLinkerSizeComparison=true 
+pushd .\bin\Debug\netcoreapp2.2\linux-arm\publish
+c:\work\putty\pscp -pw harlequin -v -r .\* pi@harlequin.local:/home/pi/Work/dotnet/TestPi.ConsoleApp
+popd
