4cd887691647b72b108b6b192a2632e6949a3abf
Kash <sergio.castillo@pronet-ise.com>
2019-08-14 11:57:28 +0200

    Probando todos los leds

--- TestPi.ConsoleApp/Program.cs
+++ TestPi.ConsoleApp/Program.cs
@@ -12,12 +12,18 @@ namespace TestPi.ConsoleApp
 
         static void Main(string[] args)
         {
-            var pin = 17;
+            var led1 = 4;
+            var led2 = 17;
+            var led3 = 27;
+            var led4 = 5;
             var controller = new GpioController();
-            controller.OpenPin(pin, PinMode.Output);
-
             var lightTimeInMilliseconds = 1000;
             var dimTimeInMilliseconds = 200;
+
+            controller.OpenPin(led1, PinMode.Output);
+            controller.OpenPin(led2, PinMode.Output);
+            controller.OpenPin(led3, PinMode.Output);
+            controller.OpenPin(led4, PinMode.Output);
 
             Console.WriteLine( "**************************************************************************************");
             Console.WriteLine($"   Framework: {RuntimeInformation.FrameworkDescription}");
@@ -31,16 +37,37 @@ namespace TestPi.ConsoleApp
 
             while (true)
             {
-                Console.WriteLine($"Light for {lightTimeInMilliseconds}ms");
-                controller.Write(pin, PinValue.High);
+                Console.WriteLine($"Light 1 for {lightTimeInMilliseconds}ms");
+                controller.Write(led1, PinValue.High);
                 Thread.Sleep(lightTimeInMilliseconds);
-                Console.WriteLine($"Dim for {dimTimeInMilliseconds}ms");
-                controller.Write(pin, PinValue.Low);
+                Console.WriteLine($"Dim 1 for {dimTimeInMilliseconds}ms");
+                controller.Write(led1, PinValue.Low);
                 Thread.Sleep(dimTimeInMilliseconds);
+                
+                Console.WriteLine($"Light 2 for {lightTimeInMilliseconds}ms");
+                controller.Write(led2, PinValue.High);
+                Thread.Sleep(lightTimeInMilliseconds);
+                Console.WriteLine($"Dim 2 for {dimTimeInMilliseconds}ms");
+                controller.Write(led2, PinValue.Low);
+                Thread.Sleep(dimTimeInMilliseconds);
+                
+                Console.WriteLine($"Light 3 for {lightTimeInMilliseconds}ms");
+                controller.Write(led3, PinValue.High);
+                Thread.Sleep(lightTimeInMilliseconds);
+                Console.WriteLine($"Dim 3 for {dimTimeInMilliseconds}ms");
+                controller.Write(led3, PinValue.Low);
+                Thread.Sleep(dimTimeInMilliseconds);
+                
+                Console.WriteLine($"Light 4 for {lightTimeInMilliseconds}ms");
+                controller.Write(led4, PinValue.High);
+                Thread.Sleep(lightTimeInMilliseconds);
+                Console.WriteLine($"Dim 4 for {dimTimeInMilliseconds}ms");
+                controller.Write(led4, PinValue.Low);
+                Thread.Sleep(dimTimeInMilliseconds);
+
                 if (CPUTemperatureSensor.IsAvailable)
                 {
-                    Console.WriteLine($"The CPU temperature is {CPUTemperatureSensor.Temperature.Celsius}");
-                    //Thread.Sleep(5000);
+                    Console.WriteLine($"The CPU temperature (Celsius) is {CPUTemperatureSensor.Temperature.Celsius}");
                 }
             } 
         }
